6962e30678f49e62cdc2fb013f264d69404ba82a
chenbang <chenbang@hzyushi.cn>
2020-05-04 16:26:37 +0800

    test

--- src/main/java/cn/com/search/web/ElasticSearchController.java
+++ src/main/java/cn/com/search/web/ElasticSearchController.java
@@ -2,6 +2,7 @@ package cn.com.search.web;
 
 import java.util.List;
 
+import com.cb.springbootjdbc.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +27,22 @@ public class ElasticSearchController {
 	ReadBooksService booksService;
 	@Autowired
 	ElasticSearchService esSeachService;
+	@Autowired
+	private HelloService helloService;
+
+	@RequestMapping("/hellotest")
+	public Result index(){
+		String sayHello = helloService.sayHello();
+		System.out.println(sayHello);
+		return  ResultGenerator.genSuccessResult();
+	}
+
+
+	@GetMapping("/hello")
+	public Result hello() {
+		return ResultGenerator.genSuccessResult();
+	}
+
 
 	@GetMapping("/creatIndex")
 	public Result creatIndex() {
@@ -41,4 +58,48 @@ public class ElasticSearchController {
 		List<BookSearchResultVo> books = esSeachService.queryDocumentByParam("test-es", "test-type", bookSearchParam);
 		return ResultGenerator.genSuccessResult(books);
 	}
+
+
+	public static void main(String[] args) {
+		int[] array1 = new int[]{5};
+		System.out.println(bsearch(array1,0,array1.length-1,5));
+		System.out.println(search(array1,5));
+
+	}
+
+
+	private static int bsearch(int[] a, int low,int hight, int value) {
+		if (low <= hight) {
+			int middle = low + ((hight - low) >> 1);
+			if(a[middle] == value){
+				return middle;
+			}else if(a[middle] < value){
+				return bsearch(a, middle + 1, hight, value);
+			}else if(a[middle] > value){
+				return bsearch(a, low,middle-1, value);
+			}
+		}
+		return -1;
+	}
+
+	public static int search(int[] nums, int target) {
+		int low = 0;
+		int hight = nums.length - 1;
+
+		while (low <= hight) {
+			int mid = (low + hight) >> 1;
+			if (nums[mid] < target) {
+				low = mid + 1;
+			} else if (nums[mid] > target) {
+				hight = mid - 1;
+			} else {
+				return mid;
+			}
+
+		}
+		return -1;
+	}
+
+
+
 }
